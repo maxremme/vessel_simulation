f314aadfcb8ab6bd7e49b178e70a3c6ea7fecf9b
Maxremmo <max.remme@gmail.com>
2024-10-06 22:07:38 +0200

    nicer output formatting

--- kafka_reader.py
+++ kafka_reader.py
@@ -1,23 +1,52 @@
 from quixstreams import Application
 import json
 
+
+def print_sea_info(msg):
+    # extract relevant info
+    current_data = msg["current"]
+
+    wave_height = current_data.get("wave_height", "N/A")
+    wave_direction = current_data.get("wave_direction", "N/A")
+    ocean_current_velocity = current_data.get("ocean_current_velocity", "N/A")
+    ocean_current_direction = current_data.get("ocean_current_direction", "N/A")
+
+    # print current conditions
+    print(f"Wave Height: {wave_height} m")
+    print(f"Wave Direction: {wave_direction}°")
+    print(f"Ocean Current Velocity: {ocean_current_velocity} km/h")
+    print(f"Ocean Current Direction: {ocean_current_direction}°")
+
+
 app = Application(
     broker_address="localhost:9092",
     loglevel="DEBUG",
     consumer_group="maritime_data_reader",
+    auto_offset_reset="latest",
 )
 
-with app.get_consumer() as consumer:
-    consumer.subscribe(["maritime_data_for_simulation"])
 
-    while True:
-        msg = consumer.poll(1)
-        if msg is None:
-            print("Waiting...")
-        elif msg.error() is not None:
-            raise Exception(msg.error())
-        else:
-            key = msg.key().decode("utf8")
-            value = json.loads(msg.value())
-            offset = msg.offset()
-            print(f"{offset} {key} {value}")
+def main():
+
+    with app.get_consumer() as consumer:
+        consumer.subscribe(["maritime_data_for_simulation"])
+
+        while True:
+            msg = consumer.poll(1)
+            if msg is None:
+                print("Waiting...")
+            elif msg.error() is not None:
+                raise Exception(msg.error())
+            else:
+                key = msg.key().decode("utf8")
+                value = json.loads(msg.value())
+                offset = msg.offset()
+                print(f"Message Offset: {offset}, Key: {key}")
+                print_sea_info(value)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
