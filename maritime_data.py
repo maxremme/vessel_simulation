e12e545e423a07603c0d349cc3372a8aff6a2609
Maxremmo <max.remme@gmail.com>
2024-10-06 16:47:23 +0200

    first commit

---
+++ maritime_data.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from quixstreams import Application
+
+response = requests.get(
+    "https://marine-api.open-meteo.com/v1/marine",
+    params={
+        "latitude": "54.544587",
+        "longitude": "10.227487",
+        "current": "wave_height",
+    },
+)
+
+maritime_data = response.json()
+
+app = Application(
+    broker_address="localhost:9092",
+    loglevel="DEBUG",
+)
+
+with app.get_producer() as producer:
+    producer.produce(
+        topic="maritime_data_test",
+        key="Gedser",
+        value=json.dumps(maritime_data),
+    )
