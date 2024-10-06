7bde6e79480a1e71a2634f4944430c9f2402353d
Maxremmo <max.remme@gmail.com>
2024-10-07 00:25:53 +0200

    added location

--- kafka_reader.py
+++ kafka_reader.py
@@ -5,13 +5,15 @@ import json
 def print_sea_info(msg):
     # extract relevant info
     current_data = msg["current"]
-
+    latitude = msg["latitude"]
+    longitude = msg["longitude"]
     wave_height = current_data.get("wave_height", "N/A")
     wave_direction = current_data.get("wave_direction", "N/A")
     ocean_current_velocity = current_data.get("ocean_current_velocity", "N/A")
     ocean_current_direction = current_data.get("ocean_current_direction", "N/A")
 
     # print current conditions
+    print(f"Location: {latitude}N,{longitude}E")
     print(f"Wave Height: {wave_height} m")
     print(f"Wave Direction: {wave_direction}°")
     print(f"Ocean Current Velocity: {ocean_current_velocity} km/h")
@@ -43,6 +45,7 @@ def main():
                 offset = msg.offset()
                 print(f"Message Offset: {offset}, Key: {key}")
                 print_sea_info(value)
+                consumer.store_offsets(msg)
 
 
 if __name__ == "__main__":
